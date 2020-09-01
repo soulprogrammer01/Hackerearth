fc8e54c97a047d84461a831179d1895560ed7798
Mayank Mishra <53926096+soulprogrammer01@users.noreply.github.com>
2020-09-01 23:45:27 +0530

    Add files via upload

---
+++ FSQRT.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+    long long int t;
+    cin>>t;
+    while(t--)
+    {
+        long long int a;
+        cin>>a;
+        cout<<floor(sqrt(a))<<endl;
+    }
+
+return 0;
+}
