b2dac015d71785fbb0256b69955fa2da4174c412
Nikhil Dubey <55599127+nikhil948@users.noreply.github.com>
2020-10-17 23:27:35 +0530

    Updated Array Introdubtion.cpp

--- Arrays Introduction.cpp
+++ Arrays Introduction.cpp
@@ -4,15 +4,16 @@ using namespace std;
   
 int main() 
 { 
-    int x;
-     int arr[10000]; 
+    long long int x;
+    long long int arr[10000]; 
     cin>>x;
-  for (int i = 0; i <= x; i++){
-      cin>>arr[i];
-  }
-   reverse(arr, arr + x);
+    for (int i = 0; i < x; i++)
+    {
+        cin>>arr[i];
+    }
+    reverse(arr, arr + x);
 
     for (int i = 0; i < x; i++) 
-        cout << arr[i]<<" ";
-    return 0; 
+        cout << arr[i] <<" ";
+return 0; 
 } 
